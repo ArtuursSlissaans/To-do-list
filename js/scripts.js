1431cc67af82bd1cf8d4571d1b562d6f5c721d88
Arturs Slisans <aslisans22@gmail.com>
2021-04-20 17:50:42 +0300

    Es kaut kur tiko nokdijos un nevaru atrast kur xD

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,37 @@
 const POP_UP = document.getElementById('popUp');
-let biblioteka = []
+let ToDoListi = [];
 
 document.getElementById('PievienotToDoListu').addEventListener('click', () => {
-    POP_UP.style.display = 'block'
+    POP_UP.style.display = 'block';
 })
 
 document.getElementById('PieveinotJaunuToDo').addEventListener('click', () => {
-    POP_UP.style.display = 'none'
+    POP_UP.style.display = 'none';
 
     let ToDoList = {KoTuDarīsi: KoTuDarīsi.value, Cikos: Cikos.value};
 
-    console.log(ToDoList)
+    KoTuDarīsi.value = "";
+    Cikos.value = "";
+
+    ToDoListi.push(ToDoList);
+
+    render();
 })
+
+
+function render() {
+    let biblioteka = document.getElementById('biblioteka');
+    biblioteka.innerHTML = "";
+
+    for(let i = 0; i < ToDoListi.length; i++) {
+        console.log(ToDoList[i]);
+        let ToDoList = `
+        <div class="ToDoList">
+             <h3>KoTuDarīsi: ${ToDoList[i].KoTuDarīsi}</h3>
+             <h4>Cikos: ${ToDoList[i].Cikos}</h4>
+    
+    </div>`;
+        biblioteka.innerHTML += ToDoList;
+    
+    }
+}
